89fefbab506805a646e4eb12465ba00c7f0b636e
Sri Ranga <sriranga451@gmail.com>
2023-01-23 20:59:09 +0530

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def HomePageView():
+def HomePageView(request):
     return HttpResponse("Hello Ranga")
